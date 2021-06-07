8f03d178b2533e56dca05be113d7f1244336ee1c
Alex Knopach <alexanderknopach@gmail.com>
2021-06-07 13:05:41 +0300

    feat: Implement IntToRoman and FillWithColor tasks

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,27 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        
+        let numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        let romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        
+        var resultStr = ""
+        var initialInt = self
+        
+        while initialInt > 0 {
+            for (index, number) in numbers.enumerated() {
+                if initialInt - number >= 0 {
+                    initialInt -= number
+                    resultStr += romans[index]
+                    break
+                }
+            }
+        }
+        
+        if resultStr == "" {
+            return nil
+        }
+        
+        return resultStr
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -2,7 +2,61 @@ import Foundation
 
 final class FillWithColor {
     
+    var visited: [[Bool]] = [[]]
+    
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        let rowsCount = image.count
+        let columnsCount = image[0].count
+        
+        if (image.first == nil || row >= rowsCount || column >= columnsCount || row < 0 || column < 0) {
+            return image
+        }
+        
+        var resultImage = image
+        let initialColor = image[row][column]
+        
+        visited = Array(repeating: Array(repeating: false, count: columnsCount), count: rowsCount)
+        visited[row][column] = true
+        resultImage[row][column] = newColor
+        checkNeighbor(&resultImage, row, column, initialColor, newColor)
+        
+        return resultImage
     }
+    
+    func checkNeighbor(_ image: inout [[Int]], _ row: Int, _ column: Int, _ initialColor: Int, _ newColor: Int) {
+        
+//        check top neighbor
+        if row != 0 {
+            if (!visited[row - 1][column] && image[row - 1][column] == initialColor) {
+                image[row - 1][column] = newColor
+                checkNeighbor(&image, row - 1, column, initialColor, newColor)
+            }
+        }
+        
+//        check bottom neighbor
+        if row < image.count - 1 {
+            if (!visited[row + 1][column] && image[row + 1][column] == initialColor) {
+                image[row + 1][column] = newColor
+                checkNeighbor(&image, row + 1, column, initialColor, newColor)
+            }
+        }
+        
+//        check left neighbor
+        if column != 0 {
+            if (!visited[row][column - 1] && image[row][column - 1] == initialColor) {
+                image[row][column - 1] = newColor
+                checkNeighbor(&image, row, column - 1, initialColor, newColor)
+            }
+        }
+        
+//        check right neighbor
+        if column < image[0].count - 1 {
+            if (!visited[row][column + 1] && image[row][column + 1] == initialColor) {
+                image[row][column + 1] = newColor
+                checkNeighbor(&image, row, column + 1, initialColor, newColor)
+            }
+        }
+        
+    }
+    
 }
